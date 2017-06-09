9420aae5563b415d176b72679cb31e3daad86ba7
nakanwagi <magwang1@gmail.com>
2017-06-09 09:25:24 -0500

    edited 5_29_2017 5_30_2017

---
+++ 5_29_2017/RatPos_and_eigenvectors_new2.m
@@ -0,0 +1,481 @@
+% Script by Mary Sylvia Agwang
+%% Make a scree plot to see if you can guess the effective dimension
+lambda = mapping2.val; % eigenvalues of the kernel matrix sorted in descending order.
+ind = mapping2.index; %indices of the eigenvalues
+% figure;
+% plot(ind, lambda, 'ko-');
+% xlabel('index');ylabel('eigenvalue');
+% title('Scree plot: 2011Data with 32 cells');
+% grid on; 
+% 
+% %print the plot
+% print('Scree Plot','-dpng');
+% 
+% %% find out how much variance the first k eigenvectors explain
+% k=2;
+% eigvec_no = 2;
+% explained_variance = 100*cumsum(lambda(1:eigvec_no))/sum(lambda(1:eigvec_no));
+% %%var = mean(var)*100; %percentage of variance explained by first k eigenvectors
+
+%% First Convert the eigenvectors to tsd objects stored in cells
+eigVec = cell(1, size(mappedX2,2));
+for i = 1:size(mappedX2, 2) % i runs over the columns of mappedX2
+    eigVec{i} = tsd(time, mappedX2(:,i));
+end
+
+
+%% Defines position in terms of cosine and sine.
+tsd_theta = tsd(time, theta');
+x = cos(mod(tsd_theta.data, 2*pi));
+y = sin(mod(tsd_theta.data, 2*pi));
+
+
+%define max and min for input in histcn
+xb = linspace(min(x), max(x), 2*size(mappedX2,2)); %range for x
+yb = linspace(min(y), max(y), 2*size(mappedX2,2)); %range for y
+
+
+% plot the rat position using only the top eigenvector
+figure;
+plot3(time, x, y, 'b');
+title('Rat position in blue:Synthetic Data');
+
+figure;
+plot3(x, y, eigVec{1}.data, 'r');
+xlabel('x');ylabel('y');zlabel('First Eigen Vector');
+title('Rat Position and  Eigvec 1');
+
+
+% print the plot
+print('Rat Position and EigVector 1','-dpng');
+
+
+%% estimate joint histogram by accumulating ,means of using eigvec 1 over rat position
+
+jointHist1 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{1}.data, 'FUN', @mean); 
+
+jointHist2 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{2}.data, 'FUN', @mean); 
+
+jointHist3 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{3}.data, 'FUN', @mean); 
+
+jointHist4 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{4}.data, 'FUN', @mean); 
+
+
+jointHist5 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{5}.data, 'FUN', @mean); 
+
+
+jointHist6 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{6}.data, 'FUN', @mean); 
+
+
+
+jointHist7 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{7}.data, 'FUN', @mean); 
+
+
+jointHist8 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{8}.data, 'FUN', @mean); 
+
+
+jointHist9 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{9}.data, 'FUN', @mean); 
+
+
+jointHist10 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{10}.data, 'FUN', @mean); 
+
+
+jointHist11 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{11}.data, 'FUN', @mean); 
+
+
+jointHist12 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{12}.data, 'FUN', @mean); 
+
+
+
+jointHist13 =  histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{13}.data, 'FUN', @mean); 
+
+
+jointHist14 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{14}.data, 'FUN', @mean); 
+
+
+jointHist15 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{15}.data, 'FUN', @mean); 
+
+
+jointHist16 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{16}.data, 'FUN', @mean); 
+
+
+jointHist17 =  histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{17}.data, 'FUN', @mean); 
+
+
+
+jointHist18 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{18}.data, 'FUN', @mean); 
+
+
+
+jointHist19 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{19}.data, 'FUN', @mean); 
+
+
+jointHist20 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{20}.data, 'FUN', @mean); 
+
+
+jointHist21 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{21}.data, 'FUN', @mean); 
+
+
+jointHist22 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{22}.data, 'FUN', @mean); 
+
+
+jointHist23 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{23}.data, 'FUN', @mean); 
+
+
+jointHist24 =  histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{24}.data, 'FUN', @mean); 
+
+
+jointHist25 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{25}.data, 'FUN', @mean); 
+
+
+jointHist26 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{26}.data, 'FUN', @mean); 
+
+
+jointHist27 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{27}.data, 'FUN', @mean); 
+
+
+jointHist28 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{28}.data, 'FUN', @mean); 
+
+
+jointHist29 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{29}.data, 'FUN', @mean); 
+
+
+jointHist30 = histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{30}.data, 'FUN', @mean); 
+
+
+jointHist31 =  histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{31}.data, 'FUN', @mean); 
+
+
+jointHist32 =  histcn([x, y], xb, yb,...,
+    'AccumData', eigVec{32}.data, 'FUN', @mean); 
+
+
+
+
+%% plot the histogram without zero values
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b1 = subplot(2, 2, 1);
+jointHist1(jointHist1==0)= nan; 
+pcolor(jointHist1);
+colorbar;
+title(b1, 'Position and Eigev 1');
+
+b2 = subplot(2, 2, 2);
+jointHist2(jointHist2==0)= nan; 
+pcolor(jointHist2);
+colorbar;
+title(b2, 'Position and Eigev 2');
+
+b3 = subplot(2, 2, 3);
+jointHist3(jointHist3==0)= nan; 
+pcolor(jointHist3);
+colorbar;
+title(b3, 'Position and Eigev 3');
+
+
+b4 = subplot(2, 2, 4);
+jointHist4(jointHist4==0)= nan; 
+pcolor(jointHist4);
+colorbar;
+title(b4, 'Position and Eigev 4');
+
+print('sublot1','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b5 = subplot(2, 2, 1);
+jointHist5(jointHist5==0)= nan; 
+pcolor(jointHist5);
+colorbar;
+title(b5, 'Position and Eigev 5');
+
+b6 = subplot(2, 2, 2);
+jointHist6(jointHist6==0)= nan; 
+pcolor(jointHist6);
+colorbar;
+title(b6, 'Position and Eigev 6');
+
+b7 = subplot(2, 2, 3);
+jointHist7(jointHist7==0)= nan; 
+pcolor(jointHist7);
+colorbar;
+title(b7, 'Position and Eigev 7');
+
+
+b8 = subplot(2, 2, 4);
+jointHist8(jointHist8==0)= nan; 
+pcolor(jointHist8);
+colorbar;
+title(b8, 'Position and Eigev 8');
+
+print('Subplot 2','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b9 = subplot(2, 2, 1);
+jointHist9(jointHist9==0)= nan; 
+pcolor(jointHist9);
+colorbar;
+title(b9, 'Position and Eigev 9');
+
+b10 = subplot(2, 2, 2);
+jointHist10(jointHist10==0)= nan; 
+pcolor(jointHist10);
+colorbar;
+title(b10, 'Position and Eigev 10');
+
+b11 = subplot(2, 2, 3);
+jointHist11(jointHist11==0)= nan; 
+pcolor(jointHist11);
+colorbar;
+title(b11, 'Position and Eigev 11');
+
+
+b12 = subplot(2, 2, 4);
+jointHist12(jointHist12==0)= nan; 
+pcolor(jointHist12);
+colorbar;
+title(b12, 'Position and Eigev 12');
+
+print('Subplot 3','-dpng');
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b13 = subplot(2, 2, 1);
+jointHist13(jointHist13==0)= nan; 
+pcolor(jointHist13);
+colorbar;
+title(b13, 'Position and Eigev 13');
+
+b14 = subplot(2, 2, 2);
+jointHist14(jointHist14==0)= nan; 
+pcolor(jointHist14);
+colorbar;
+title(b14, 'Position and Eigev 14');
+
+b15 = subplot(2, 2, 3);
+jointHist15(jointHist15==0)= nan; 
+pcolor(jointHist15);
+colorbar;
+title(b15, 'Position and Eigev 15');
+
+
+b16 = subplot(2, 2, 4);
+jointHist16(jointHist16==0)= nan; 
+pcolor(jointHist16);
+colorbar;
+title(b16, 'Position and Eigev 16');
+
+print('Subplot 4','-dpng');
+
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b17 = subplot(2, 2, 1);
+jointHist17(jointHist17==0)= nan; 
+pcolor(jointHist17);
+colorbar;
+title(b17, 'Position and Eigev 17');
+
+b18 = subplot(2, 2, 2);
+jointHist18(jointHist18==0)= nan; 
+pcolor(jointHist18);
+colorbar;
+title(b18, 'Position and Eigev 18');
+
+b19 = subplot(2, 2, 3);
+jointHist19(jointHist19==0)= nan; 
+pcolor(jointHist19);
+colorbar;
+title(b19, 'Position and Eigev 19');
+
+
+b20 = subplot(2, 2, 4);
+jointHist20(jointHist20==0)= nan; 
+pcolor(jointHist20);
+colorbar;
+title(b20, 'Position and Eigev 20');
+
+print('Subplot 5','-dpng');
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b21 = subplot(2, 2, 1);
+jointHist21(jointHist21==0)= nan; 
+pcolor(jointHist21);
+colorbar;
+title(b21, 'Position and Eigev 21');
+
+b22 = subplot(2, 2, 2);
+jointHist22(jointHist22==0)= nan; 
+pcolor(jointHist22);
+colorbar;
+title(b22, 'Position and Eigev 22');
+
+b23 = subplot(2, 2, 3);
+jointHist23(jointHist23==0)= nan; 
+pcolor(jointHist23);
+colorbar;
+title(b23, 'Position and Eigev 23');
+
+
+b24 = subplot(2, 2, 4);
+jointHist24(jointHist24==0)= nan; 
+pcolor(jointHist24);
+colorbar;
+title(b24, 'Position and Eigev 24');
+
+print('Subplot 6','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b25 = subplot(2, 2, 1);
+jointHist25(jointHist25==0)= nan; 
+pcolor(jointHist25);
+colorbar;
+title(b25, 'Position and Eigev 25');
+
+b26 = subplot(2, 2, 2);
+jointHist26(jointHist26==0)= nan; 
+pcolor(jointHist26);
+colorbar;
+title(b26, 'Position and Eigev 26');
+
+b27 = subplot(2, 2, 3);
+jointHist27(jointHist27==0)= nan; 
+pcolor(jointHist27);
+colorbar;
+title(b27, 'Position and Eigev 27');
+
+
+b28 = subplot(2, 2, 4);
+jointHist28(jointHist28==0)= nan; 
+pcolor(jointHist28);
+colorbar;
+title(b28, 'Position and Eigev 28');
+
+print('Subplot 7','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b29 = subplot(2, 2, 1);
+jointHist29(jointHist29==0)= nan; 
+pcolor(jointHist29);
+colorbar;
+title(b29, 'Position and Eigev 29');
+
+
+b30 = subplot(2, 2, 2);
+jointHist30(jointHist30==0)= nan; 
+pcolor(jointHist30);
+colorbar;
+title(b30, 'Position and Eigev 30');
+
+b31 = subplot(2, 2, 3);
+jointHist31(jointHist31==0)= nan; 
+pcolor(jointHist31);
+colorbar;
+title(b31, 'Position and Eigev 31');
+
+
+b32 = subplot(2, 2, 4);
+jointHist32(jointHist32==0)= nan; 
+pcolor(jointHist32);
+colorbar;
+title(b32, 'Position and Eigev 32');
+
+print('Subplot 8','-dpng');
+
+
+%% Compute the Mutual_Information for each of the 32 eigenvectors
+
+
+iV = 32;
+% I have only put 10 because it's easy to see the contributions of
+% each eigenvector on submatrix plot
+eigVecb = linspace(min(eigVec{iV}.data), max(eigVec{iV}.data), length(time));
+%compute a 3D hsitogram to estimate the joint entropy
+H = histcn([x, y, eigVec{iV}.data],xb, yb, eigVecb);
+% compute the normalizing constant;
+
+%% Matricize the tensor to speed up MATLAB computations
+
+Hnew = reshape(H, [], size(H,3)); %convert to 64^2*length(x.data)
+% remove zeros so that log2 is defined
+Hnew(Hnew==0)=nan;
+
+
+Hnew = Hnew./nansum(nansum(nansum(Hnew)));
+
+%%
+
+lh1 = log2(nansum(Hnew,1));
+lh2 = log2(nansum(Hnew,2));
+MutualInf32 = nansum(nansum(Hnew .* bsxfun(@minus,bsxfun(@minus,log2(Hnew),lh1),lh2)));
+
+%% Create a Mutual Information vector.
+
+MutualInfVector = [MutualInf1, MutualInf2,  MutualInf3,  MutualInf4,...
+    MutualInf5, MutualInf6,  MutualInf7, MutualInf8, MutualInf9,...
+    MutualInf10, MutualInf11, MutualInf12, MutualInf13, MutualInf14,...
+    MutualInf15,  MutualInf16,  MutualInf17, MutualInf18,  MutualInf19,...
+    MutualInf20, MutualInf21,  MutualInf22, MutualInf23,  MutualInf24,...
+   MutualInf25, MutualInf26,  MutualInf27,  MutualInf28,  MutualInf29,...
+   MutualInf30, MutualInf31,  MutualInf32];
+
+%%  
+figure;
+p = plot(MutualInfVector);
+p.Marker = 'square';
+xlabel('Eigen vector index'); ylabel('Mutual Information');
+title('Mutual Information Vs EigenVector index');
+grid on;
+
+print('Mutual Information','-dpng');
+% 
+% 
+% %use I(X,Y) = -sum(sum(p(x,y)log(p(x,y)/(p(x)*p(y)))))
+% % I(X,Y) = -sum(sum(p(x,y)[log(p(x,y)-log(p(x))-log(p(y))]).
+% 
+
+
+
+
+
--- 5_30_2017_realData_analysis/RatPos_and_eigenvectors.m
+++ 5_30_2017_realData_analysis/RatPos_and_eigenvectors.m
@@ -1,68 +1,459 @@
-%% Script to visualize how the eigenvectors reconstruct the rat position
-% load('R232-2011-10-16-vt.mat'); %this loads the rat positions in 2011 data
-% whos
-
-%% Convert the eigenvectors to ts objects
-x1 = tsd(time, mappedX2(:,1)); %first largest eigenvector
-y1 = tsd(time, mappedX2(:,2)); %second largest eigen vector
-new_angle = atan2(mappedX2(:,1), mappedX2(:,2));
-
-%%Make a scree plot to see if you can guess the effective dimension
+% Script by Mary Sylvia Agwang
+%% Make a scree plot to see if you can guess the effective dimension
 lambda = mapping2.val; % eigenvalues of the kernel matrix sorted in descending order.
 ind = mapping2.index; %indices of the eigenvalues
 figure;
 plot(ind, lambda, 'ko-');
 xlabel('index');ylabel('eigenvalue');
 title('Scree plot: 2011Data with 32 cells');
+grid on; 
 
+%print the plot
+print('Scree Plot','-dpng');
+
+%% find out how much variance the first k eigenvectors explain
+k=2;
+eigvec_no = 2;
+explained_variance = 100*cumsum(lambda(1:eigvec_no))/sum(lambda(1:eigvec_no));
+%%var = mean(var)*100; %percentage of variance explained by first k eigenvectors
+
+%% First Convert the eigenvectors to tsd objects stored in cells
+eigVec = cell(1, size(mappedX2,2));
+for i = 1:size(mappedX2, 2) % i runs over the columns of mappedX2
+    eigVec{i} = tsd(time, mappedX2(:,i));
+end
+
+
+% new_angle = atan2(mappedX2(:,1), mappedX2(:,2));
 
 %% plot the rat position using only the top eigenvector
 figure;
-plot3(x.data, y.data, x.data(x.range), 'g');
+a1 = subplot (2, 1, 1);
+plot3(x.data, y.data, x.data(x.range), 'b');
 hold on;
-plot3(x.data, y.data,  x1.data(x.range), 'r');
-xlabel('x');ylabel('y');zlabel('range of x')
-title('Rat position in green, top eigenvector only in  red: 2011Data');
+plot3(x.data, y.data,  eigVec{1}.data(x.range), 'r');
+xlabel('x');ylabel('y');zlabel('First Eigen Vector');
+title(a1, 'Rat position in blue, first eigenvector only in  red: 2011Data');
+
+
+a2 = subplot(2, 1, 2);
+plot3(x.data, y.data, x.data(x.range), 'b');
+hold on;
+plot3(x.data, y.data,  eigVec{5}.data(x.range), 'r');
+xlabel('x');ylabel('y');zlabel('First Eigen Vector');
+title(a2, 'Position Eigvec 5');
 hold off;
 
-%% ???To compute mutual info, we need to do a histogram-based estimation
-% method to estimate the joint estimated probability distribution of the 
-% the two random variables
-
-%FORMULA: I(X,Y) = H(X) + H(Y) - H(X,Y)
-% H(x) = -sum p(x) log2(p(x))---marginal pdf of x.
-% why log 2 and not ln?
-
-bound1 = linspace(min(x.data), max(x.data), 80); %range for x
-bound2 = linspace(min(y.data), max(y.data), 80); %range for y
-
-jointHist = histcn([x.data y.data], bound1, bound2,...,
-    'AccumData', x1.data(x.range), 'FUN', @mean); %estimate joint pdf using eigvec 1
-
-%make log2 defined by setting zero values in the jointHist equal to nan
-% jointHistogram(jointHistogram == 0) = nan; 
-idx = unique(jointHist);
+% print the plot
+print('Rat Position and EigVector 1 and 5','-dpng');
 
 
-%plot the histogram without zero values
+
+
+%% estimate joint histogram by accumulating ,means of using eigvec 1 over rat position
+
+jointHist1 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{1}.data(x.range), 'FUN', @mean); 
+
+jointHist2 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{2}.data(x.range), 'FUN', @mean); 
+
+jointHist3 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{3}.data(x.range), 'FUN', @mean);
+
+jointHist4 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{4}.data(x.range), 'FUN', @mean);
+
+jointHist5 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{5}.data(x.range), 'FUN', @mean); 
+
+jointHist6 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{6}.data(x.range), 'FUN', @mean); 
+
+
+jointHist7 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{7}.data(x.range), 'FUN', @mean); 
+
+jointHist8 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{8}.data(x.range), 'FUN', @mean); 
+
+
+jointHist9 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{9}.data(x.range), 'FUN', @mean); 
+
+
+
+jointHist10 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{10}.data(x.range), 'FUN', @mean); 
+
+jointHist11 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{11}.data(x.range), 'FUN', @mean); 
+
+jointHist12 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{12}.data(x.range), 'FUN', @mean); 
+
+
+jointHist13 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{13}.data(x.range), 'FUN', @mean); 
+
+jointHist14 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{14}.data(x.range), 'FUN', @mean); 
+
+jointHist15 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{15}.data(x.range), 'FUN', @mean); 
+
+jointHist16 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{16}.data(x.range), 'FUN', @mean); 
+
+jointHist17 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{17}.data(x.range), 'FUN', @mean); 
+
+
+jointHist18 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{18}.data(x.range), 'FUN', @mean); 
+
+
+jointHist19 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{19}.data(x.range), 'FUN', @mean); 
+
+jointHist20 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{20}.data(x.range), 'FUN', @mean); 
+
+jointHist21 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{21}.data(x.range), 'FUN', @mean); 
+
+jointHist22 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{22}.data(x.range), 'FUN', @mean); 
+
+jointHist23 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{23}.data(x.range), 'FUN', @mean); 
+
+jointHist24 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{24}.data(x.range), 'FUN', @mean); 
+
+jointHist25 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{25}.data(x.range), 'FUN', @mean); 
+
+jointHist26 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{26}.data(x.range), 'FUN', @mean); 
+
+jointHist27 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{27}.data(x.range), 'FUN', @mean); 
+
+jointHist28 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{28}.data(x.range), 'FUN', @mean); 
+
+jointHist29 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{29}.data(x.range), 'FUN', @mean); 
+
+jointHist30 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{30}.data(x.range), 'FUN', @mean); 
+
+jointHist31 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{31}.data(x.range), 'FUN', @mean); 
+
+jointHist32 = histcn([x.data y.data], xb, yb,...,
+    'AccumData', eigVec{32}.data(x.range), 'FUN', @mean); 
+
+
+
+%% plot the histogram without zero values
 figure;
-jointHist(jointHist==0)= nan; %set zero values in jointhistogram to nan
-pcolor(jointHist);
+%set zero values in jointhistogram to nan so log2 is defined
+b1 = subplot(2, 2, 1);
+jointHist1(jointHist1==0)= nan; 
+pcolor(jointHist1);
 colorbar;
-title('Rat Position based on the first largest eigenvector only');
+title(b1, 'Position and Eigev 1');
+
+b2 = subplot(2, 2, 2);
+jointHist2(jointHist2==0)= nan; 
+pcolor(jointHist2);
+colorbar;
+title(b2, 'Position and Eigev 2');
+
+b3 = subplot(2, 2, 3);
+jointHist3(jointHist3==0)= nan; 
+pcolor(jointHist3);
+colorbar;
+title(b3, 'Position and Eigev 3');
 
 
-%%  Find the joint probability distribution with zero values removed.
-jointProbDist = jointHist./nansum(jointHist); %joint probability distribution
-jointEntropy = -nansum(jointHist(:).* log2(jointHist(:)));
+b4 = subplot(2, 2, 4);
+jointHist4(jointHist4==0)= nan; 
+pcolor(jointHist4);
+colorbar;
+title(b4, 'Position and Eigev 4');
 
-% can one assume that X=[x.data y.data] is in rows so that
-% marginal = nansum(JointHist, 1); and then repeat the computation?
+print('sublot1','-dpng');
 
-%most code assumes vectors contain integers but mine are continuous
-%variables.
-% is your code set up to address that
-% are you familair with spiketrain analysis tool?
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b5 = subplot(2, 2, 1);
+jointHist5(jointHist5==0)= nan; 
+pcolor(jointHist5);
+colorbar;
+title(b5, 'Position and Eigev 5');
+
+b6 = subplot(2, 2, 2);
+jointHist6(jointHist6==0)= nan; 
+pcolor(jointHist6);
+colorbar;
+title(b6, 'Position and Eigev 6');
+
+b7 = subplot(2, 2, 3);
+jointHist7(jointHist7==0)= nan; 
+pcolor(jointHist7);
+colorbar;
+title(b7, 'Position and Eigev 7');
+
+
+b8 = subplot(2, 2, 4);
+jointHist8(jointHist8==0)= nan; 
+pcolor(jointHist8);
+colorbar;
+title(b8, 'Position and Eigev 8');
+
+print('Subplot 2','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b9 = subplot(2, 2, 1);
+jointHist9(jointHist9==0)= nan; 
+pcolor(jointHist9);
+colorbar;
+title(b9, 'Position and Eigev 9');
+
+b10 = subplot(2, 2, 2);
+jointHist10(jointHist10==0)= nan; 
+pcolor(jointHist10);
+colorbar;
+title(b10, 'Position and Eigev 10');
+
+b11 = subplot(2, 2, 3);
+jointHist11(jointHist11==0)= nan; 
+pcolor(jointHist11);
+colorbar;
+title(b11, 'Position and Eigev 11');
+
+
+b12 = subplot(2, 2, 4);
+jointHist12(jointHist12==0)= nan; 
+pcolor(jointHist12);
+colorbar;
+title(b12, 'Position and Eigev 12');
+
+print('Subplot 3','-dpng');
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b13 = subplot(2, 2, 1);
+jointHist13(jointHist13==0)= nan; 
+pcolor(jointHist13);
+colorbar;
+title(b13, 'Position and Eigev 13');
+
+b14 = subplot(2, 2, 2);
+jointHist14(jointHist14==0)= nan; 
+pcolor(jointHist14);
+colorbar;
+title(b14, 'Position and Eigev 14');
+
+b15 = subplot(2, 2, 3);
+jointHist15(jointHist15==0)= nan; 
+pcolor(jointHist15);
+colorbar;
+title(b15, 'Position and Eigev 15');
+
+
+b16 = subplot(2, 2, 4);
+jointHist16(jointHist16==0)= nan; 
+pcolor(jointHist16);
+colorbar;
+title(b16, 'Position and Eigev 16');
+
+print('Subplot 4','-dpng');
+
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b17 = subplot(2, 2, 1);
+jointHist17(jointHist17==0)= nan; 
+pcolor(jointHist17);
+colorbar;
+title(b17, 'Position and Eigev 17');
+
+b18 = subplot(2, 2, 2);
+jointHist18(jointHist18==0)= nan; 
+pcolor(jointHist18);
+colorbar;
+title(b18, 'Position and Eigev 18');
+
+b19 = subplot(2, 2, 3);
+jointHist19(jointHist19==0)= nan; 
+pcolor(jointHist19);
+colorbar;
+title(b19, 'Position and Eigev 19');
+
+
+b20 = subplot(2, 2, 4);
+jointHist20(jointHist20==0)= nan; 
+pcolor(jointHist20);
+colorbar;
+title(b20, 'Position and Eigev 20');
+
+print('Subplot 5','-dpng');
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b21 = subplot(2, 2, 1);
+jointHist21(jointHist21==0)= nan; 
+pcolor(jointHist21);
+colorbar;
+title(b21, 'Position and Eigev 21');
+
+b22 = subplot(2, 2, 2);
+jointHist22(jointHist22==0)= nan; 
+pcolor(jointHist22);
+colorbar;
+title(b22, 'Position and Eigev 22');
+
+b23 = subplot(2, 2, 3);
+jointHist23(jointHist23==0)= nan; 
+pcolor(jointHist23);
+colorbar;
+title(b23, 'Position and Eigev 23');
+
+
+b24 = subplot(2, 2, 4);
+jointHist24(jointHist24==0)= nan; 
+pcolor(jointHist24);
+colorbar;
+title(b24, 'Position and Eigev 24');
+
+print('Subplot 6','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b25 = subplot(2, 2, 1);
+jointHist25(jointHist25==0)= nan; 
+pcolor(jointHist25);
+colorbar;
+title(b25, 'Position and Eigev 25');
+
+b26 = subplot(2, 2, 2);
+jointHist26(jointHist26==0)= nan; 
+pcolor(jointHist26);
+colorbar;
+title(b26, 'Position and Eigev 26');
+
+b27 = subplot(2, 2, 3);
+jointHist27(jointHist27==0)= nan; 
+pcolor(jointHist27);
+colorbar;
+title(b27, 'Position and Eigev 27');
+
+
+b28 = subplot(2, 2, 4);
+jointHist28(jointHist28==0)= nan; 
+pcolor(jointHist28);
+colorbar;
+title(b28, 'Position and Eigev 28');
+
+print('Subplot 7','-dpng');
+
+
+figure;
+%set zero values in jointhistogram to nan so log2 is defined
+b29 = subplot(2, 2, 1);
+jointHist29(jointHist29==0)= nan; 
+pcolor(jointHist29);
+colorbar;
+title(b29, 'Position and Eigev 29');
+
+
+b30 = subplot(2, 2, 2);
+jointHist30(jointHist30==0)= nan; 
+pcolor(jointHist30);
+colorbar;
+title(b30, 'Position and Eigev 30');
+
+b31 = subplot(2, 2, 3);
+jointHist31(jointHist31==0)= nan; 
+pcolor(jointHist31);
+colorbar;
+title(b31, 'Position and Eigev 31');
+
+
+b32 = subplot(2, 2, 4);
+jointHist32(jointHist32==0)= nan; 
+pcolor(jointHist32);
+colorbar;
+title(b32, 'Position and Eigev 32');
+
+print('Subplot 8','-dpng');
+
+%% Recover the Rat position using the first eigenvector only.
+
+xb = linspace(min(x.data), max(x.data), 2*size(mappedX2,2)); %range for x
+yb = linspace(min(y.data), max(y.data), 2*size(mappedX2,2)); %range for y
+
+
+
+
+%% Compute the Mutual_Information for each of the 32 eigenvectors
+
+iV = 32;
+% I have only put 10 because it's easy to see the contributions of
+% each eigenvector on submatrix plot
+eigVecb = linspace(min(eigVec{iV}.data), max(eigVec{iV}.data), length(x.range));
+%compute a 3D hsitogram to estimate the joint entropy
+H = histcn([x.data, y.data, eigVec{iV}.data(x.range)],xb, yb, eigVecb);
+% compute the normalizing constant;
+
+%% Matricize the tensor to speed up MATLAB computations
+
+Hnew = reshape(H, [], size(H,3)); %convert to 64^2*length(x.data)
+% remove zeros so that log2 is defined
+Hnew(Hnew==0)=nan;
+
+
+Hnew = Hnew./nansum(nansum(nansum(Hnew)));
+
+%%
+
+lh1 = log2(nansum(Hnew,1));
+lh2 = log2(nansum(Hnew,2));
+MutualInf32 = nansum(nansum(Hnew .* bsxfun(@minus,bsxfun(@minus,log2(Hnew),lh1),lh2)));
+
+%% Create a Mutual Information vector.
+
+MutualInfVector = [MutualInf1, MutualInf2,  MutualInf3,  MutualInf4,...
+    MutualInf5, MutualInf6,  MutualInf7, MutualInf8, MutualInf9,...
+    MutualInf10, MutualInf11, MutualInf12, MutualInf13, MutualInf14,...
+    MutualInf15,  MutualInf16,  MutualInf17, MutualInf18,  MutualInf19,...
+    MutualInf20, MutualInf21,  MutualInf22, MutualInf23,  MutualInf24,...
+   MutualInf25, MutualInf26,  MutualInf27,  MutualInf28,  MutualInf29,...
+   MutualInf30, MutualInf31,  MutualInf32];
+
+%%  
+figure;
+p = plot(MutualInfVector);
+p.Marker = 'square';
+xlabel('Eigen vector index'); ylabel('Mutual Information');
+title('Mutual Information Vs EigenVector index');
+grid on;
+
+print('Mutual Information','-dpng');
+
+
+%use I(X,Y) = -sum(sum(p(x,y)log(p(x,y)/(p(x)*p(y)))))
+% I(X,Y) = -sum(sum(p(x,y)[log(p(x,y)-log(p(x))-log(p(y))]).
+
 
 
 
